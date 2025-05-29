57e27ef8f02767d10973d62e6d6784eae263e9f6
Daniele Cardullo <danielecardullo.drive@gmail.com>
2025-05-29 16:36:04 +0200

    kaggle

--- train_and_test.sh
+++ train_and_test.sh
@@ -3,5 +3,3 @@
 for split in A B C D; do
     python dl_hackathon/main.py --train-path data/$split/train.json.gz --test-path data/$split/test.json.gz --epochs 10
 done
-
-python fl_hackathon/zipthefolder.py
